a1dcbed5d4fc6f77de28917e598120b1f4863623
unknown <hitaliabgul@gmail.com>
2024-05-08 17:38:59 +0100

    frontend done

--- backend/Routes/Route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getFood } from "../Controller/Foodcontroller";
-
-const router=express.Router()
-
-router.get("/",getFood);
-
-export default router;
--- backend/Routes/Userroute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { login, signup } from "../Controller/Usercontroller.js";
-const router=express.Router()
-
-router.post("/signup",signup);
-router.post("/login",login);
-
-export default router;
